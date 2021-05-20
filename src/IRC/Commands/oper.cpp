8f04fa8c035ca95a1d964c2f02ef769140aaa71a
mbrunel <mbrunel@student.42.fr>
2021-05-20 17:23:58 +0200

    moi d'abord

--- src/IRC/Commands/oper.cpp
+++ src/IRC/Commands/oper.cpp
@@ -6,7 +6,7 @@
 /*   By: mbrunel <mbrunel@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/05/20 15:32:22 by mbrunel           #+#    #+#             */
-/*   Updated: 2021/05/20 16:30:15 by mbrunel          ###   ########.fr       */
+/*   Updated: 2021/05/20 17:14:18 by mbrunel          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,11 +19,11 @@ int IrcServer::oper(User &u, const Message &m)
 		return (writeNum(u, IrcError::notregistered()));
 	if (m.params().size() < 2)
 		return (writeNum(u, IrcError::needmoreparams(m.command())));
-	Oper o = network.getOper(m.params()[0]);
-	if (o.host.size() && o.host != u.socket()->host())
-		return (writeNum(u, IrcError::nooperhost()));
-	if (o.pass != m.params()[1])
+	Oper *o = network.getOper(m.params()[0]);
+	if (!o || o->pass != m.params()[1])
 		return (writeNum(u, IrcError::passwdmissmatch()));
+	if (o->host.size() && o->host != u.socket()->host())
+		return (writeNum(u, IrcError::nooperhost()));
 	writeNum(u, IrcReply::youreoper());
 	return true;
 }
