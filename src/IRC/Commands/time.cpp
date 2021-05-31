a49cece5b319e6f10f80f676d94a48ee320d6d9b
user42 <mapapin@student.42.fr>
2021-05-31 16:10:29 +0200

    fixIssueRegistration

--- src/IRC/Commands/time.cpp
+++ src/IRC/Commands/time.cpp
@@ -6,7 +6,7 @@
 /*   By: mapapin <mapapin@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/05/24 17:07:05 by mapapin           #+#    #+#             */
-/*   Updated: 2021/05/24 17:07:09 by mapapin          ###   ########.fr       */
+/*   Updated: 2021/05/31 16:07:10 by mapapin          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,6 +14,8 @@
 
 int IrcServer::time(User &u, const Message &m)
 {
+	if (!u.isRegistered())
+		return (writeNum(u, IrcError::notregistered()));
     // Change the 'if' below when the multi-server is managed
     if (m.params().size())
         return (writeNum(u, IrcError::nosuchserver(m.params()[0])));
