2744df942cff0a811f1913b6c1dad49c28f68f63
mapapin <56489418+mapapin@users.noreply.github.com>
2021-06-10 14:28:43 +0200

    Modifs search Kick + fix error compilation PART (#60)

--- src/IRC/Commands/part.cpp
+++ src/IRC/Commands/part.cpp
@@ -6,7 +6,7 @@
 /*   By: mbrunel <mbrunel@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/04/01 16:40:29 by asoursou          #+#    #+#             */
-/*   Updated: 2021/06/09 16:32:40 by mbrunel          ###   ########.fr       */
+/*   Updated: 2021/06/10 12:09:07 by mapapin          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -34,7 +34,7 @@ int IrcServer::part(User &u, const Message &m)
 			msg << *chan;
 			if (m.params().size() > 1)
 				msg << m.params()[1];
-			c->send()(msg.str());
+			c->send(msg.str());
 			c->delMember(&u);
 			if (!c->count())
 				network.remove(c);
