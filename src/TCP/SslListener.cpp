38c39ffbd61ccbfc9b5948a0ebf880348c7d7cc6
Adrien Soursou <mbrunel@student.42.fr>
2021-06-11 15:36:00 +0200

    delete headers

--- src/TCP/SslListener.cpp
+++ src/TCP/SslListener.cpp
@@ -1,15 +1,3 @@
-/* ************************************************************************** */
-/*                                                                            */
-/*                                                        :::      ::::::::   */
-/*   SslListener.cpp                                    :+:      :+:    :+:   */
-/*                                                    +:+ +:+         +:+     */
-/*   By: mbrunel <mbrunel@student.42.fr>            +#+  +:+       +#+        */
-/*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2021/03/09 02:11:27 by mbrunel           #+#    #+#             */
-/*   Updated: 2021/03/15 00:50:54 by mbrunel          ###   ########.fr       */
-/*                                                                            */
-/* ************************************************************************** */
-
 #include "SslListener.hpp"
 
 SslListener::SslListener(int family, SSL_CTX *ctx):Listener(family), _ctx(ctx) {}
