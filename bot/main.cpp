5cdf271d0e93c54fdb38ced3e0f0a7d1817bd265
Adrien Soursou <asoursou@student.42.fr>
2021-06-20 21:00:14 +0200

    Fix #119

--- bot/main.cpp
+++ bot/main.cpp
@@ -201,7 +201,7 @@ private:
 
 	int roll(const IRC::Param &nick, const Params &)
 	{
-		writeLine(nick, ft::to_string(std::rand() % 6));
+		writeLine(nick, ft::to_string((std::rand() % 6) + 1));
 		return (0);
 	}
 
